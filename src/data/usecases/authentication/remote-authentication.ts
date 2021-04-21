02fd5ee4ca60b57f498a9bd6f23d5398e0436523
Breno Henrique Martins Xavier <breno.xavier@invillia.com>
2021-04-21 00:49:40 -0300

    feat: enruse RemoTEauthentication trhwos invalidCredentials if credentials was invalid

--- src/data/usecases/authentication/remote-authentication.ts
+++ src/data/usecases/authentication/remote-authentication.ts
@@ -1,4 +1,6 @@
 import { HttpPostClient } from '@/data/protocols/http/http-post-client'
+import { HttpStatusCode } from '@/data/protocols/http/http-response'
+import { InvalidCredentialsError } from '@/domain/erros/invalid-credentials-error'
 import { Authentication, AuthenticationParams } from '@/domain/usecases/authentication'
 class RemoteAuthentication {
   constructor(
@@ -8,10 +10,19 @@ class RemoteAuthentication {
 
   async auth (params: AuthenticationParams): Promise<void> {
 
-    await this.httpPostClient.post({
+    const { statusCode } = await this.httpPostClient.post({
       url: this.url,
       body: params
     })
+
+    switch (statusCode) {
+      case HttpStatusCode.unathorized:
+        throw new InvalidCredentialsError()
+        
+      default:
+        return Promise.resolve()
+    }
+
   }
 }
 
