b72eb769ed70a5b9ae2a67176a31619297e36dc5
Breno Henrique Martins Xavier <breno.xavier@invillia.com>
2021-04-21 01:41:52 -0300

    feat: ensure RemoteAuthentication return an account if httpPostClient returns 200

--- src/data/usecases/authentication/remote-authentication.ts
+++ src/data/usecases/authentication/remote-authentication.ts
@@ -4,21 +4,21 @@ import { InvalidCredentialsError, UnexpectedError } from '@/domain/erros'
 import { Authentication, AuthenticationParams } from '@/domain/usecases/authentication'
 import { AccountModel } from '@/domain/models/account-model'
 
-class RemoteAuthentication {
+class RemoteAuthentication implements Authentication {
   constructor (
     private readonly url: string,
     private readonly httpPostClient: HttpPostClient<AuthenticationParams, AccountModel>
   ) { }
 
-  async auth (params: AuthenticationParams): Promise<void> {
-    const { statusCode } = await this.httpPostClient.post({
+  async auth (params: AuthenticationParams): Promise<AccountModel> {
+    const { statusCode, body } = await this.httpPostClient.post({
       url: this.url,
       body: params
     })
 
     switch (statusCode) {
       case HttpStatusCode.ok:
-        break
+        return body
 
       case HttpStatusCode.unathorized:
         throw new InvalidCredentialsError()
