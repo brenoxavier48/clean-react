96545ea9a1ae3bec96985efc4a0ee49dd7fa1766
Breno Henrique Martins Xavier <brenoxavier8.bx@gmail.com>
2021-04-21 23:31:07 -0300

    refactor: create a function for make HttpPostParams

--- src/infra/http/axios-http-client.spec.ts
+++ src/infra/http/axios-http-client.spec.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import faker from 'faker'
 import { AxiosHttpClient } from './axios-http-client'
-import {} from '@/data/protocols/http'
+import { HttpPostParams } from '@/data/protocols/http'
 
 jest.mock('axios')
 const mockedAxios = axios as jest.Mocked<typeof axios>
@@ -10,9 +10,14 @@ const makeSut = (): AxiosHttpClient => {
   return new AxiosHttpClient()
 }
 
+const makeHttpPostParams = (): HttpPostParams<any> => ({
+  url: faker.internet.url(),
+  body: faker.random.objectElement()
+})
+
 describe('AxiosHttpClient', () => {
   test('Should call axios with correct url and verb', async () => {
-    const url = faker.internet.url()
+    const { url } = makeHttpPostParams()
     const sut = makeSut()
     sut.post({ url })
     expect(mockedAxios.post).toHaveBeenCalledWith(url)
