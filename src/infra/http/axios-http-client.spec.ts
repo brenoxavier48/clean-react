16b3d77743f4a96e1f0d570544783599c7988003
Breno Henrique Martins Xavier <brenoxavier8.bx@gmail.com>
2021-04-22 22:44:01 -0300

    refactor: separate mock factories

--- src/infra/http/axios-http-client.spec.ts
+++ src/infra/http/axios-http-client.spec.ts
@@ -2,40 +2,36 @@ import axios from 'axios'
 import faker from 'faker'
 import { AxiosHttpClient } from './axios-http-client'
 import { HttpPostParams } from '@/data/protocols/http'
+import { mockHttpPostParams } from '@/data/tests'
+import { makeAxios } from '@/infra/test'
 
 jest.mock('axios')
 
-const mockedAxios = axios as jest.Mocked<typeof axios>
-const mockedAxiosReturn = {
-  data: faker.random.objectElement(),
-  status: ''
-}
-mockedAxios.post.mockResolvedValue(mockedAxiosReturn)
-
-const makeSut = (): AxiosHttpClient => {
-  return new AxiosHttpClient()
+interface SutTypes {
+  sut: AxiosHttpClient,
+  mockedAxios: jest.Mocked<typeof axios>
 }
 
-const makeHttpPostParams = (): HttpPostParams<any> => ({
-  url: faker.internet.url(),
-  body: faker.random.objectElement()
-})
+const makeSut = (): SutTypes => {
+  return {
+    sut: new AxiosHttpClient(),
+    mockedAxios: makeAxios()
+  }
+}
 
 describe('AxiosHttpClient', () => {
   test('Should call axios with correct values', async () => {
-    const requestParams = makeHttpPostParams()
-    const sut = makeSut()
+    const requestParams = mockHttpPostParams()
+    const { sut, mockedAxios } = makeSut()
     sut.post(requestParams)
     expect(mockedAxios.post).toHaveBeenCalledWith(requestParams.url, requestParams.body)
   })
 
   test('Should return the correct values', async () => {
-    const requestParams = makeHttpPostParams()
-    const sut = makeSut()
-    const returnedValues = await sut.post(requestParams)
-    expect(returnedValues).toEqual({
-      statusCode: mockedAxiosReturn.status,
-      body: mockedAxiosReturn.data
-    })
+    const requestParams = mockHttpPostParams()
+    const { sut, mockedAxios } = makeSut()
+    const returnedValues = sut.post(requestParams)
+    const expectedValues = mockedAxios.post.mock.results[0].value
+    expect(returnedValues).toEqual(expectedValues)
   })
 })
