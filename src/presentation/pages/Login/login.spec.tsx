b43b90d64d2a2ebe68002ebfb424ce43f3994625
Breno Henrique Martins Xavier <brenoxavier8.bx@gmail.com>
2021-04-30 23:03:40 -0300

    refactor: move sut creatoion to a factory method

--- src/presentation/pages/Login/login.spec.tsx
+++ src/presentation/pages/Login/login.spec.tsx
@@ -1,17 +1,27 @@
 import React from 'react'
 import "@testing-library/jest-dom"
-import { render, screen } from '@testing-library/react'
+import { render, RenderResult, screen } from '@testing-library/react'
 import Login from './Login'
+
+type SutTypes = {
+  sut: RenderResult
+}
+
+const makeSut = (): SutTypes => {
+  return {
+    sut: render(<Login />)
+  }
+}
 
 describe('Login component', () => {
   test('Ensure Login component does\'t show Error and Spinner at the beginning', async () => {
-    render(<Login />)
+    makeSut()
     const errorWrap = screen.getByTestId('error-wraper')
     expect(errorWrap.childElementCount).toBe(0)
   })
 
   test('Ensure submit button is disabled at the begging', async () => {
-    render(<Login />)
+    makeSut()
     const submitButton = screen.getByTestId('submit-button')
     expect(submitButton).toBeDisabled()
   })
